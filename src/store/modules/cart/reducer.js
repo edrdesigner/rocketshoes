da2910af526f5f6255e7b78fca37b80252cafcf6
eduardo.reichert <eduardo.reichert@wssim.com.br>
2019-08-20 17:24:21 -0300

    first commit

---
+++ src/store/modules/cart/reducer.js
@@ -0,0 +1,31 @@
+import produce from 'immer';
+
+export default function cart(state = [], action) {
+  switch (action.type) {
+    case '@cart/ADD_SUCESS':
+      return produce(state, draft => {
+        const { product } = action;
+
+        draft.push(product);
+      });
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const productIdex = draft.findIndex(p => p.id === action.id);
+
+        if (productIdex >= 0) {
+          draft.splice(productIdex, 1);
+        }
+      });
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
+      return produce(state, draft => {
+        const productIdex = draft.findIndex(p => p.id === action.id);
+
+        if (productIdex >= 0) {
+          draft[productIdex].amount = Number(action.amount);
+        }
+      });
+    }
+    default:
+      return state;
+  }
+}
